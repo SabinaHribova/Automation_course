736b513da866b4721fe1f4406597753ae55d0951
SabinaHribova <136639190+SabinaHribova@users.noreply.github.com>
2023-09-20 16:40:46 +0200

    health_check stage 2
    The health_check.py is almost done. It was simply tested on the command line. It checks cpu, memory and disk, calls the main function every 60 seconds a sends the email, if something is wrong. 
    Few things are still missing - documentation and the local host check

--- health_check.py
+++ health_check.py
@@ -9,18 +9,54 @@
 
 import psutil
 import shutil
+#import emails
+import time
 
-summary = []
+cpu_message = 'Error - CPU usage is over 80 %'
+mem_message = 'Error - Available memory is less than 500MB'
+disk_message = 'Error - Available disk space is less than 20%'
+
+summary = [cpu_message, mem_message, disk_message]
 
 #TO DO: definovat funkce pro kontrolu všech veličin. Chybové hlášky uložit do proměnné
 def cpu_check():
   cpu = psutil.cpu_percent(5)
-  if cpu > 80:
-    summary.append('Error - CPU usage is over 80 %')
+  if cpu < 80:
+    summary.remove(cpu_message)
 
-#TO DO: zavolat všechny funkce. Každých 60 sekund
+def memory_check():
+  mem = psutil.virtual_memory() #svmem(total=16349069312, available=5441630208, percent=66.7, used=10907439104, free=5441630208) in bytes
+  if mem[1]/1000000 > 500:
+    summary.remove(mem_message)
 
-#TO DO pokud funkce najdou chybu, poslat email.
+def disk_check():
+  disk = psutil.disk_usage('/') #sdiskusage(total=21378641920, used=4809781248, free=15482871808, percent=22.5)
+  if 100 - disk[3]  > 20:
+    summary.remove(disk_message) 
+
+def main():
+  cpu_check()
+  memory_check()
+  disk_check() 
+
+sender = "automation@example.com"
+receiver = "username@example.com"
+subject = '\n'.join(summary)
+body = "Please check your system and resolve the issue as soon as possible."
+
+starttime = time.time()
+
+while True:
+    print("tick")
+    main ()
+    print(summary)
+    #if len(summary) >= 1:
+      #message = emails.generate(sender, receiver, subject, body)
+      #emails.send(message)
+    # Remove the Time taken by code to execute
+    time.sleep(20.0 - ((time.time() - starttime) % 20.0))
+    summary = [cpu_message, mem_message, disk_message]
+
 
 
 
